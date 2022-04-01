8bc137bf669c3d090b0e0671633012a7cdfdc574
Esther Carminatti <yunalesca002@gmail.com>
2022-04-01 14:42:48 +0100

    basic functionality done

---
+++ 03.unit-converter/index.js
@@ -0,0 +1,39 @@
+// Use a common class for all the 20 (user input)
+// Get the user input.(standar input to begin with)
+// Create functions with the convertion of the user input (e.g. meters to feet and viceversa)
+// Display them inside the html elements.
+
+let userInput = document.getElementById("unit").textContent = 18;
+let error = document.getElementById("error");
+
+function length() {
+    let meterToFeet = userInput * 3.28084
+    let feetToMeter = userInput * 0.3048
+    let length = document.getElementById("length")
+    length.textContent = `${userInput} meters = ${meterToFeet.toFixed(3)} feet | ${userInput} feet = ${feetToMeter.toFixed(3)} meters`
+};
+
+function volume() {
+    let litresToGallons = userInput * 0.264172
+    let gallonsToLitres = userInput * 3.785412
+    let volume = document.getElementById("volume")
+    volume.textContent = `${userInput} litres = ${litresToGallons.toFixed(3)} gallons | ${userInput} gallons = ${gallonsToLitres.toFixed(3)} litres`
+};
+
+function mass() {
+    let kiloToPounds = userInput * 2.2
+    let poundsToKilo = userInput / 2.2046
+    let mass = document.getElementById("mass")
+    mass.textContent = `${userInput} kilos = ${kiloToPounds.toFixed(3)} pounds | ${userInput} pounds = ${poundsToKilo.toFixed(3)} kilos`
+};
+
+function errorMessage() {
+    if (userInput.match(/^((?!(abc|def)).)*$/) ) {
+        return error.textContent = "You need to write a valid number"
+    }
+};
+
+length()
+volume()
+mass()
+errorMessage()
