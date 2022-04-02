a372b112e993fcfadf7a6047a08f8d75661eb4a8
Esther Carminatti <yunalesca002@gmail.com>
2022-04-03 00:07:33 +0100

    converter completed

--- 03.unit-converter/index.js
+++ 03.unit-converter/index.js
@@ -1,41 +1,36 @@
-// Get the user input.(standar input to begin with)
-// Create functions with the convertion of the user input (e.g. meters to feet and viceversa)
-// Display them inside the html elements.
+// Initial values
+length(0)
+volume(0)
+mass(0)
 
-// let userInput = document.getElementById("unit").textContent = 18;
-
-let error = document.getElementById("error");
-let userInput = document.getElementById("submitbutton").onclick = function() {
-  return parseInt(document.getElementById("myText").value)
+// Getting the value from the user and passing it to functions
+function getVal() {
+  val = document.querySelector('input').value;
+  length(val);
+  volume(val);
+  mass(val);
 }
 
-function length() {
-    let meterToFeet = userInput * 3.28084
-    let feetToMeter = userInput * 0.3048
-    let length = document.getElementById("length")
-    length.textContent = `${userInput} meters = ${meterToFeet.toFixed(3)} feet | ${userInput} feet = ${feetToMeter.toFixed(3)} meters`
+function length(userInput) {
+  let meterToFeet = userInput * 3.28084;
+  let feetToMeter = userInput * 0.3048;
+  let length = document.getElementById("length");
+  length.textContent = `${userInput} meters = ${meterToFeet.toFixed(3)} feet |
+                        ${userInput} feet = ${feetToMeter.toFixed(3)} meters`;
 };
 
-function volume() {
-    let litresToGallons = userInput * 0.264172
-    let gallonsToLitres = userInput * 3.785412
-    let volume = document.getElementById("volume")
-    volume.textContent = `${userInput} litres = ${litresToGallons.toFixed(3)} gallons | ${userInput} gallons = ${gallonsToLitres.toFixed(3)} litres`
+function volume(userInput) {
+  let litresToGallons = userInput * 0.264172;
+  let gallonsToLitres = userInput * 3.785412;
+  let volume = document.getElementById("volume");
+  volume.textContent = `${userInput} litres = ${litresToGallons.toFixed(3)} gallons |
+                        ${userInput} gallons = ${gallonsToLitres.toFixed(3)} litres`;
 };
 
-function mass() {
-    let kiloToPounds = userInput * 2.2
-    let poundsToKilo = userInput / 2.2046
-    let mass = document.getElementById("mass")
-    mass.textContent = `${userInput} kilos = ${kiloToPounds.toFixed(3)} pounds | ${userInput} pounds = ${poundsToKilo.toFixed(3)} kilos`
+function mass(userInput) {
+  let kiloToPounds = userInput * 2.2;
+  let poundsToKilo = userInput / 2.2046;
+  let mass = document.getElementById("mass");
+  mass.textContent = `${userInput} kilos = ${kiloToPounds.toFixed(3)} pounds |
+                      ${userInput} pounds = ${poundsToKilo.toFixed(3)} kilos`;
 };
-
-function errorMessage() {
-    if (userInput.match(/^((?!(abc|def)).)*$/) ) {
-        return error.textContent = "You need to write a valid number"
-    }
-};
-
-length()
-volume()
-mass()
